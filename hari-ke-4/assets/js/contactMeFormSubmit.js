5a3da581c575c04bf044b77cff56235472d657cf
udenbaguse <udenbaguselord@gmail.com>
2026-01-31 09:25:51 +0700

    add: finish task 4

---
+++ hari-ke-4/assets/js/contactMeFormSubmit.js
@@ -0,0 +1,31 @@
+import { showAlert } from "./alert.js";
+import { validateForm, setupRealtimeValidation } from "./validation.js";
+
+const form = document.getElementById("formNama");
+const alertBox = document.getElementById("alertBox");
+
+// Setup realtime validation
+setupRealtimeValidation(form);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formValid = validateForm(form);
+
+  if (formValid) {
+    showAlert({
+      alertBox,
+      type: "success",
+      message: "<strong>Done!</strong> invoice will be sent via email.",
+    });
+
+    // form.reset();
+  } else {
+    showAlert({
+      alertBox,
+      type: "danger",
+      message:
+        "<strong>Failed!</strong> check the red fields.",
+    });
+  }
+});
